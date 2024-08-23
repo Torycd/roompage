3951074d111636c78ca377281856640712cedf80
Torycd <vicidundun@gmail.com>
2024-08-23 12:43:59 +0100

    leggo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 // import React from 'react'
 
-import Hero from "./components/Hero"
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <div className="sm:max-h-screen w-full">
-      <Hero/>
+      <div className="row-span-2">
+        <Hero />
+      </div>
+      <div className="row-span-1">
+        <p>The email</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,22 @@
 // import React from 'react'
+import Image3 from "../assets/images/desktop-image-hero-3.jpg"
 
 const Hero = () => {
   return (
     <div className="grid grid-cols-3">
-        <div className="col-span-2 p-20">
-            <div></div>
+      <div className="col-span-2">
+        <div className="w-full h-full group relative bg-gray-700 parent-container   overflow-hidden">
+          <img
+            className="opacity-50 object-cover w-full h-full"
+            src={Image3}
+            alt="Image Picture"
+          />
+          <div className="absolute top-[50%] space-y-4 left-[50%] translate-x-[-50%] translate-y-[-50%]"></div>
         </div>
-        <div className="col-span-1">
-
-        </div>
+      </div>
+      <div className="col-span-1"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
