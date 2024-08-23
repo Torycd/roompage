b1e761a089127e0dfad8365aaae611590a3a3ef6
Torycd <vicidundun@gmail.com>
2024-08-23 01:17:19 +0100

    lets begin

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 // import React from 'react'
 
+import Hero from "./components/Hero"
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="sm:max-h-screen w-full">
+      <Hero/>
+    </div>
   )
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="grid grid-cols-3">
+        <div className="col-span-2 p-20">
+            <div></div>
+        </div>
+        <div className="col-span-1">
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
