1398d50ebd6baa09eb420560abbaac6ad03181fa
Torycd <vicidundun@gmail.com>
2024-08-29 08:30:20 +0100

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div className="sm:max-h-screen w-full">
-      <div className="row-span-2">
+    <div className="h-screen">
+      <div className="h-[73%] w-full">
         <Hero />
       </div>
-      <div className="row-span-1">
+      <div className="h-[27%] bg-gray-950 w-full">
         <p>The email</p>
       </div>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,44 @@
-// import React from 'react'
-import Image3 from "../assets/images/desktop-image-hero-3.jpg"
+import desktop1 from "../assets/images/desktop-image-hero-1.jpg";
 
 const Hero = () => {
   return (
-    <div className="grid grid-cols-3">
-      <div className="col-span-2">
-        <div className="w-full h-full group relative bg-gray-700 parent-container   overflow-hidden">
-          <img
-            className="opacity-50 object-cover w-full h-full"
-            src={Image3}
-            alt="Image Picture"
-          />
-          <div className="absolute top-[50%] space-y-4 left-[50%] translate-x-[-50%] translate-y-[-50%]"></div>
+    <div className="flex h-full">
+      <div
+        className="p-5 text-white font-bold w-[65%] bg-cover bg-center bg-no-repeat"
+        style={{
+          backgroundImage: `url(${desktop1})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="flex space-x-5 items-center">
+          <div className="flex items-center justify-center">
+            <h2 className="text-4xl">ROOM</h2>
+          </div>
+
+          <ul className="flex space-x-5">
+            <li>Home</li>
+            <li>SHop</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
         </div>
       </div>
-      <div className="col-span-1"></div>
+      <div className="w-[40%]">
+        <div className="p-20 space-y-5">
+          <h2 className="text-5xl font-bold">
+            Discover innovative ways to decorate
+          </h2>
+          <p className="text-wrap">
+            We provide unmatched qaunity, comfort, and style fro property
+            Object.getOwnProperties across the couuntry We provide unmatched
+            qaunity, comfort, and style fro property Object.getOwnProperties
+            across the couuntry We provide unmatched qaunity, comfort, and style
+            fro property Object.
+          </p>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
